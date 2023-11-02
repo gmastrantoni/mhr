da4d11e34b431f045c8715635bf0e45415fc4f5e
Giandomenico Mastrantoni <75527253+gmastrantoni@users.noreply.github.com>
2023-11-02 11:36:26 +0100

    Update mhr_analysis.py

--- code/mhr_analysis.py
+++ code/mhr_analysis.py
@@ -1,6 +1,5 @@
-@author: giand
-
-# import libraries
+@author: giandomenico mastrantoni - giandomenico.mastrantoni@uniroma1.it
+#%% import libraries
 import random
 import math
 
@@ -25,7 +24,23 @@ import contextily as cx
 os.chdir(".../data") # insert your folder path here.
 
 #%% READ PRE-PROCESSED INPUT DATA
-AOI
-OMI
-cs
-builds_w_all
+print('Reading Input Data...')
+# Area Of Interest
+AOI = gpd.read_file("AOI.gpkg", ignore_fields=['location', 'id', 'objectid', 'cod_pro','cod_istat', 'pro_com', 'nome', 'shape_leng', 'shape_area'])
+# Italian Real Estate Market Observatory (Osservatorio Mercato Immobiliare - Agenzia delle Entrate)
+OMI = gpd.read_file("ASSETS/pre-processed/OMI_AOI.gpkg")
+# Census Tracts
+cs = gpd.read_file("ASSETS/sez_censuarie_2011_Roma_32633.gpkg",
+                       ignore_fields=['pro_com', 'cod_stagno','cod_fiume', 
+                                      'cod_lago', 'cod_laguna', 'cod_val_p', 'cod_zona_c',
+                                      'cod_is_amm', 'cod_is_lac', 'cod_is_mar', 'cod_area_s', 'cod_mont_d',
+                                      'loc2011', 'cod_loc', 'tipo_loc', 'com_asc', 'cod_asc', 'ace',
+                                      'shape_leng'
+                                      ]
+                       )
+
+# Buildings from DBSN Esercito, already pre-processed (output from data_preparation.py)
+builds_w_all = gpd.read_file("ASSETS/pre-processed/builds_w_all.gpkg")
+
+
+
