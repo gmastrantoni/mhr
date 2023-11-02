81c42a0a9fbbd2ba1c9450400d5fb7dabd107099
Giandomenico Mastrantoni <75527253+gmastrantoni@users.noreply.github.com>
2023-11-02 11:29:17 +0100

    Update and rename MR_Analysis.py to mhr_analysis.py

--- code/mhr_analysis.py
+++ code/mhr_analysis.py
@@ -21,3 +21,12 @@ from matplotlib_scalebar.scalebar import ScaleBar
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import contextily as cx
+
+#%% Set main folder path
+os.chdir(".../data") # insert your folder path here.
+
+#%% READ PRE-PROCESSED INPUT DATA
+AOI
+OMI
+cs
+builds_w_all
